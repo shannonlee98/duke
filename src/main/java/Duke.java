0b8dc314a5e2679d8473edf53cbc24e8dae452a0
ShannonLee <shanklee.lj@gmail.com>
2019-08-28 00:13:26 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,34 +2,51 @@ import java.util.Scanner;
 public class Duke {
 
     private static Scanner input = new Scanner(System.in);
-    private static String list[] = new String[100];
+    private static Task[] list = new Task[100];
     private static int i = 0;
 
     public static void main(String[] args) {
-
         greet();
+//        print(Character.toString((char) 0x2713));
 
         String text = input.nextLine();
         while(!(text.contentEquals("bye"))){
-            if (text.contentEquals("list")){
+            printLine();
+            if (text.contentEquals("list")) {
                 list();
+            } else if (text.contains("done ") && text.substring(0, 5).contentEquals("done ")) {
+                done(text.substring(5));
             } else {
-                echo(text);
                 add(text);
             }
+            printLine();
             text = input.nextLine();
         }
         exit();
     }
 
+    private static void done(String text) {
+        int taskNumber = Integer.parseInt(text) - 1;
+        if (taskNumber < i) {
+            list[taskNumber].markAsDone();
+            print("Nice! I've marked this task as done: ");
+            print("[" + list[taskNumber].getStatusIcon() + "] " + list[taskNumber].description);
+        }
+    }
+
     private static void add(String text) {
-        list[i] = text;
+        Task t = new Task(text);
+        print("added: " + t.description);
+
+        list [i] = t;
         i++;
     }
 
     private static void list() {
+        print("Here are the tasks in your list:");
         for (int j = 0; j < i; j++){
-            System.out.println((char) j+1 + ". " + list[j]);
+            String text = (char) j+1 + ". [" + list[j].getStatusIcon() + "] " + list[j].description;
+            print(text);
         }
     }
 
@@ -39,22 +56,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
+        print(logo);
 
-        printLine("Hello! I'm Duke\nWhat can I do for you");
-    }
-
-    private static void echo(String text) {
-        printLine("added: " + text);
+        printLine();
+        print("Hello! I'm Duke\nWhat can I do for you");
+        printLine();
     }
 
     private static void exit() {
-        printLine("Bye. Hope to see you soon!");
+        print("Bye. Hope to see you soon!");
     }
 
-    public static void printLine(String text){
-        System.out.println("_______________________________________________");
-        System.out.println(text);
-        System.out.println("_______________________________________________");
+    public static void printLine() {
+        print("_______________________________________________");
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
     }
 }
