76da41d838a5fb38f2a215e1e6ae35f33c4f4692
ShannonLee <shanklee.lj@gmail.com>
2019-08-27 22:03:02 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,54 @@
 import java.util.Scanner;
 public class Duke {
+
+    private static Scanner input = new Scanner(System.in);
+    private static String list[] = new String[100];
+    private static int i = 0;
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        greet();
+
+        String text = input.nextLine();
+        while(!(text.contentEquals("bye"))){
+            if (text.contentEquals("list")){
+                list();
+            } else {
+                echo(text);
+                add(text);
+            }
+            text = input.nextLine();
+        }
+        exit();
+    }
+
+    private static void add(String text) {
+        list[i] = text;
+        i++;
+    }
+
+    private static void list() {
+        for (int j = 0; j < i; j++){
+            System.out.println((char) j+1 + ". " + list[j]);
+        }
+    }
+
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
 
         printLine("Hello! I'm Duke\nWhat can I do for you");
+    }
 
-        String text = input.nextLine();
-        while(!(text.contentEquals("bye"))){
-            printLine(text);
-            text = input.nextLine();
-        }
+    private static void echo(String text) {
+        printLine("added: " + text);
+    }
+
+    private static void exit() {
         printLine("Bye. Hope to see you soon!");
     }
 
